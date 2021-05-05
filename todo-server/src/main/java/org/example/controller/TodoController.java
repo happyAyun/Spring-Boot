3a5a676c25a27fa680f3ecb5866978cbf906b720
Ayun Sim <shim52111@gmail.com>
2021-05-06 01:03:22 +0900

    service & controller

--- todo-server/src/main/java/org/example/controller/TodoController.java
+++ todo-server/src/main/java/org/example/controller/TodoController.java
@@ -1,2 +1,70 @@
-package org.example.controller;public class TodoController {
+package org.example.controller;
+
+import lombok.AllArgsConstructor;
+import org.example.model.TodoEntity;
+import org.example.model.TodoRequest;
+import org.example.model.TodoResponse;
+import org.example.service.TodoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController // controller 표시
+@CrossOrigin // CORS issue를 막기 위해
+@RequestMapping("/") // base URL 설정
+@AllArgsConstructor
+
+public class TodoController {
+
+    private final TodoService service;
+
+    @PostMapping
+    public ResponseEntity<TodoResponse> create(@RequestBody TodoRequest request) {
+        System.out.println("CREATE");
+
+        // 비정상적인 요청이 될 경우
+        if (ObjectUtils.isEmpty(request.getTitle()))
+            return ResponseEntity.badRequest().build(); // 잘못된 요청이라고 응답을 내려준다
+
+        if (ObjectUtils.isEmpty(request.getOrder()))
+            request.setOrder(0L); // default 값을 준다
+
+        if (ObjectUtils.isEmpty(request.getCompleted()))
+            request.setCompleted(false);
+
+        TodoEntity result = this.service.add(request);
+        return ResponseEntity.ok(new TodoResponse(result));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<TodoResponse> readOne() {
+        System.out.println("READ ONE");
+        return null;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TodoResponse>> readAll() {
+        System.out.println("READ ALL");
+        return null;
+    }
+
+    @PatchMapping("{id}")
+    public ResponseEntity<TodoResponse> update() {
+        System.out.println("UPDATE");
+        return null;
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteOne() {
+        System.out.println("DELETE");
+        return null;
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteAll() {
+        System.out.println("DELETE ALL");
+        return null;
+    }
 }
